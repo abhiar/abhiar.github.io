f6b126e1fd66253a3ddfeab1fed8d1235eeb0d60
Abhinav <abhinavarora@live.com>
2019-12-21 01:47:19 +0530

    new website

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
-#abhiar.github.io
-
-
-Completely designed by: https://github.com/abhiar/
-
-
-Resources: 
-
-1. https://coolors.co : For color palette management
-2. https://www.pexels.com/ : For free stock background images
-3. Google fonts
-4. CDN
-5. MDN
-
+# SurrRobo
--- script.js
+++ script.js
@@ -1,70 +1,145 @@
-/*
-Linking welcome scren to main panel
-*/
-
-welcomeScreen = document.getElementById('welcome');
-welcomeScreen.style.cursor = 'pointer';
-
-welcomeScreen.onclick = function () {
-  location.href = "#";
-  location.href = "#main";
+$.get('127.0.0.1:8080', success);
+function success() {
+  console.log('wohoo');
 }
 
-/*
-Navigation Pane
-*/
+animating = 0
+positions = [[320,180], [200,300], [90,150]]
 
-function turnOn(it) {
-  elements.forEach(x => {
-    if(x===it) {
-      x.style.backgroundColor = '#394032';//'#8d99ae';
-    }
-    else {
-      x.style.backgroundColor = '#79b791';
-    }
-  });
+homeButton = document.getElementById('homeNav')
+opButton = document.getElementById('operationsNav')
+aboutButton = document.getElementById('aboutNav')
+moreButton = document.getElementById('moreNav')
+homePanel = document.getElementById('home')
+opPanel = document.getElementById('operations')
+aboutPanel = document.getElementById('about')
+morePanel = document.getElementById('more')
+marker = document.getElementById('marker')
+
+lastPanel = homePanel
+lastButton = homeButton
+homeButton.style.color = 'orange';
+
+homeButton.addEventListener('click', dispHome)
+opButton.addEventListener('click', dispOperations)
+aboutButton.addEventListener('click', dispAbout)
+moreButton.addEventListener('click', dispMore)
+
+opPanel.style.display = 'none';
+aboutPanel.style.display = 'none';
+morePanel.style.display = 'none';
+marker.style.opacity = 0;
+
+
+
+sButtons = document.getElementsByClassName('button');
+for(let i = 0; i < sButtons.length; i++) {
+  sButtons[i].addEventListener('mouseover', moveMarker);
+}
+for(let i = 0; i < sButtons.length; i++) {
+  sButtons[i].addEventListener('mouseout', resetMarker);
 }
 
-function updateNav() {
-  let scrollVal = returnScroll();
-  scalingFactor = 933/window.innerHeight;
-  scrollVal *= scalingFactor;
-
-  if(scrollVal < 925)
-    navPane.style.left = '-3%';
-  else
-    navPane.style.left = '';
-
-  if(scrollVal < 925)
-    turnOn(l1);
-  else if(scrollVal < 1842)
-    turnOn(l2);
-  else if(scrollVal < 2759)
-    turnOn(l3);
-  else if(scrollVal < 3125)
-    turnOn(l4);
-  else
-    turnOn(l5);
+function moveMarker(e) {
+  e.target.style.backgroundColor = '#803300';
+  idx = e.target.dataset.sno;
+  marker.style.marginTop = positions[idx][0];
+  marker.style.marginLeft = positions[idx][1];
+  flicker(marker);
 }
 
-function scrollToElement(event) {
-  let target = panels[event.target.id.split('')[1]-1];
-  if(target) {
-    location.href = "#";
-    location.href = panels[event.target.id.split('')[1]-1];
+function resetMarker(e) {
+  e.target.style.backgroundColor = '#404040';
+  marker.style.opacity = 0;
+}
+
+
+function dispHome(e) {
+  lastButton.style.color = 'white';
+  e.target.style.color = 'orange';
+  animate(homePanel);
+  lastPanel.style.display = 'none';
+  homePanel.style.display = 'flex';
+  lastPanel = homePanel;
+  lastButton = e.target;
+}
+
+function dispOperations(e) {
+  lastButton.style.color = 'white';
+  e.target.style.color = 'orange';
+  animate(opPanel);
+  lastPanel.style.display = 'none';
+  opPanel.style.display = 'flex';
+  lastPanel = opPanel;
+  lastButton = e.target;
+}
+
+function dispAbout(e) {
+  lastButton.style.color = 'white';
+  e.target.style.color = 'orange';
+  animate(aboutPanel);
+  lastPanel.style.display = 'none';
+  aboutPanel.style.display = 'flex';
+  lastPanel = aboutPanel;
+  lastButton = e.target;
+}
+
+function dispMore(e) {
+  lastButton.style.color = 'white';
+  e.target.style.color = 'orange';
+  animate(morePanel);
+  lastPanel.style.display = 'none';
+  morePanel.style.display = 'flex';
+  lastPanel = morePanel;
+  lastButton = e.target;
+}
+
+
+
+function max(a,b) {
+  if (a>b) {
+    return a;
+  }
+  else {
+    return b;
   }
 }
 
-const returnScroll = () => document.body.scrollTop;
-let l1 = document.getElementById('l1');
-let l2 = document.getElementById('l2');
-let l3 = document.getElementById('l3');
-let l4 = document.getElementById('l4');
-let l5 = document.getElementById('l5');
-let navPane = document.querySelector('nav');
+function animate(obj) {
+  obj.style.transform = 'scale(0)';
+  let id = setInterval(frame, 10);
+  animating = 1;
+  let i = 0
+  let t = 0
 
-elements = [l1, l2, l3, l4, l5];
-panels = ['#welcome', '#main', '#panel2', '#panel3', '#panel4'];
+  function frame() {
+    t += 0.2;
+    i = 1-Math.exp(-1*t)
+    if (i >= 0.99) {
+      clearInterval(id);
+      obj.style.transform = 'scale(1)';
+      animating = 0;
+    } else {
+      obj.style.transform = 'scale('+i+')';
+    }
+  }
+}
 
-setInterval(updateNav, 200);
-navPane.onclick = scrollToElement;
+function flicker(obj) {
+  obj.style.opacity = 0.5;
+  let id = setInterval(frame, 50);
+  i = 0;
+  function frame() {
+    obj.style.opacity = 0;
+    if (i == 1) {
+      obj.style.opacity = 0.5;
+      clearInterval(id);
+    }
+    console.log(i);
+    if (i == 1) {
+      i = 0;
+    } else {
+      i += 1;
+    }
+  }
+}
